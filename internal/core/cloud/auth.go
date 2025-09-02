7b3b6a2309c081c0abb12ad47a9614326a1ad253
linhuafeng <lpxqu@qq.com>
2025-09-02 23:02:07 +0800

    feat(auth): 添加基础认证接口及云端实现骨架
    - 新增 IAuth 和 ICommon 接口定义认证相关方法
    - 实现 cloud 包中的 Auth 结构体及方法骨架
    - 在 main 函数中实例化 Auth 并绑定至应用
    - 配置应用启动时初始化认证上下文
    - 留出 Login 和 Register 方法待后续实现

---
+++ internal/core/cloud/auth.go
@@ -0,0 +1,30 @@
+package cloud
+
+import (
+	"context"
+
+	"gitlab.linhf.cn/project/lemontea/lemon_tea_desktop/internal/core"
+	"gitlab.linhf.cn/project/lemontea/lemon_tea_desktop/rpc/service"
+)
+
+type Auth struct {
+	ctx context.Context
+}
+
+func (a *Auth) Startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+func (a *Auth) Login(request *service.LoginRequest) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (a *Auth) Register(request *service.RegisterRequest) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewAuth() core.IAuth {
+	return &Auth{}
+}
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"embed"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"gitlab.linhf.cn/project/lemontea/lemon_tea_desktop/internal/core/cloud"
 )
 
 //go:embed all:frontend/dist
@@ -13,7 +15,8 @@ var assets embed.FS
 
 func main() {
 	// Create an instance of the app structure
-	app := NewApp()
+
+	auth := cloud.NewAuth()
 
 	// Create application with options
 	err := wails.Run(&options.App{
@@ -24,9 +27,11 @@ func main() {
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		OnStartup: func(ctx context.Context) {
+			auth.Startup(ctx)
+		},
 		Bind: []interface{}{
-			app,
+			auth,
 		},
 	})
 
