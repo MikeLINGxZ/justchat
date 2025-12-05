895d4e895fea0a6276ee69cbd4102a901a1eda0c
linhuafeng <lpxqu@qq.com>
2025-12-05 16:56:43 +0800

    优化一些界面代码

--- frontend/src/components/Chat/MessageList/index.tsx
+++ frontend/src/components/Chat/MessageList/index.tsx
@@ -30,9 +30,13 @@ const MessageList:  React.FC<MessageListProps> = ({
     isLoading,
     isGenerating
 }) => {
+
+
+
     return (
         <div className={`${className} ${styles.MessageList}`}>
-            <div className={styles.Content}>
+            {/* 消息 */}
+            <div className={styles.content}>
                 {
                     messages.map((message, index) => (
                         <div>
@@ -40,6 +44,26 @@ const MessageList:  React.FC<MessageListProps> = ({
                         </div>
                     ))
                 }
+                {/* 滚动到底部按钮 */}
+                <div className={`${styles.scrollButton}`}>
+                    <div className={`${styles.scrollButtonIcon}`}>
+                        <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M7 10L12 15L17 10"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            />
+                        </svg>
+                    </div>
+                </div>
             </div>
         </div>
     )
