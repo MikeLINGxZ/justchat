a7c0fb066d3c485e6088ddd67057edab6cc50e57
linhuafeng <lpxqu@qq.com>
2025-12-16 16:59:32 +0800

    支持文件上传

--- backend/pkg/file_uploader/common.go
+++ backend/pkg/file_uploader/common.go
@@ -36,131 +36,163 @@ func NewConverter(providerModel *wrapper_models.ProviderModel) Converter {
 }
 
 func (d *Converter) ConvertMessageUserInputMultiContent(message *schema.Message) error {
+	if message == nil {
+		return nil
+	}
 
-	var uploader IFileUploader
-
-	switch d.providerModel.ProviderType {
-	case data_models.ProviderTypeAliyuns:
-		uploader = &Aliyuns{
-			providerModel: d.providerModel,
-		}
-	default:
+	uploader := d.getUploader()
+	if uploader == nil {
 		return nil
 	}
 
 	paths := d.getPaths(message)
+	if len(paths) == 0 {
+		return nil
+	}
 
 	path2url, err := uploader.Upload(paths)
 	if err != nil {
 		return err
 	}
 
-	for i, part := range message.UserInputMultiContent {
-		switch part.Type {
-		case schema.ChatMessagePartTypeImageURL:
-			if message.UserInputMultiContent[i].Image == nil || message.UserInputMultiContent[i].Image.Extra == nil {
-				continue
-			}
-			path := message.UserInputMultiContent[i].Image.Extra[ExtraKeyFilePath.String()]
-			if path == nil || path == "" {
-				continue
-			}
-			url := path2url[path.(string)]
-			if url != "" {
-				message.UserInputMultiContent[i].Image.URL = &url
-			}
-		case schema.ChatMessagePartTypeAudioURL:
-			if message.UserInputMultiContent[i].Audio == nil || message.UserInputMultiContent[i].Audio.Extra == nil {
-				continue
-			}
-			path := message.UserInputMultiContent[i].Audio.Extra[ExtraKeyFilePath.String()]
-			if path == nil || path == "" {
-				continue
-			}
-			url := path2url[path.(string)]
-			if url != "" {
-				message.UserInputMultiContent[i].Audio.URL = &url
-			}
-		case schema.ChatMessagePartTypeVideoURL:
-			if message.UserInputMultiContent[i].Video == nil || message.UserInputMultiContent[i].Video.Extra == nil {
-				continue
-			}
-			path := message.UserInputMultiContent[i].Video.Extra[ExtraKeyFilePath.String()]
-			if path == nil || path == "" {
-				continue
-			}
-			url := path2url[path.(string)]
-			if url != "" {
-				message.UserInputMultiContent[i].Video.URL = &url
-			}
-		case schema.ChatMessagePartTypeFileURL:
-			if message.UserInputMultiContent[i].File == nil || message.UserInputMultiContent[i].File.Extra == nil {
-				continue
-			}
-			path := message.UserInputMultiContent[i].File.Extra[ExtraKeyFilePath.String()]
-			if path == nil || path == "" {
-				continue
-			}
-			url := path2url[path.(string)]
-			if url != "" {
-				message.UserInputMultiContent[i].File.URL = &url
-			}
-		default:
+	for i := range message.UserInputMultiContent {
+		part := &message.UserInputMultiContent[i]
+		if !isFilePartType(part.Type) {
 			continue
 		}
+
+		path, ok := getFilePathFromPart(part)
+		if !ok {
+			continue
+		}
+
+		url := path2url[path]
+		setURLToPart(part, url)
 	}
 
 	return nil
 }
 
-func (d *Converter) getPaths(message *schema.Message) []string {
-	var paths []string
+// getUploader 根据 provider 类型创建对应的上传器
+func (d *Converter) getUploader() IFileUploader {
+	switch d.providerModel.ProviderType {
+	case data_models.ProviderTypeAliyuns:
+		return &Aliyuns{
+			providerModel: d.providerModel,
+		}
+	default:
+		return nil
+	}
+}
 
-	if message == nil {
-		return paths
+// getFilePathFromPart 从不同类型的 part 中提取文件路径
+func getFilePathFromPart(part *schema.MessageInputPart) (string, bool) {
+	var extra map[string]interface{}
+
+	switch part.Type {
+	case schema.ChatMessagePartTypeImageURL:
+		if part.Image == nil {
+			return "", false
+		}
+		extra = part.Image.Extra
+	case schema.ChatMessagePartTypeAudioURL:
+		if part.Audio == nil {
+			return "", false
+		}
+		extra = part.Audio.Extra
+	case schema.ChatMessagePartTypeVideoURL:
+		if part.Video == nil {
+			return "", false
+		}
+		extra = part.Video.Extra
+	case schema.ChatMessagePartTypeFileURL:
+		if part.File == nil {
+			return "", false
+		}
+		extra = part.File.Extra
+	default:
+		return "", false
 	}
 
-	for i, part := range message.UserInputMultiContent {
-		switch part.Type {
-		case schema.ChatMessagePartTypeImageURL:
-			if message.UserInputMultiContent[i].Image == nil || message.UserInputMultiContent[i].Image.Extra == nil {
-				continue
-			}
-			path := message.UserInputMultiContent[i].Image.Extra[ExtraKeyFilePath.String()]
-			if path == nil || path == "" {
-				continue
-			}
-			paths = append(paths, path.(string))
-		case schema.ChatMessagePartTypeAudioURL:
-			if message.UserInputMultiContent[i].Audio == nil || message.UserInputMultiContent[i].Audio.Extra == nil {
-				continue
-			}
-			path := message.UserInputMultiContent[i].Audio.Extra[ExtraKeyFilePath.String()]
-			if path == nil || path == "" {
-				continue
-			}
-			paths = append(paths, path.(string))
-		case schema.ChatMessagePartTypeVideoURL:
-			if message.UserInputMultiContent[i].Video == nil || message.UserInputMultiContent[i].Video.Extra == nil {
-				continue
-			}
-			path := message.UserInputMultiContent[i].Video.Extra[ExtraKeyFilePath.String()]
-			if path == nil || path == "" {
-				continue
-			}
-			paths = append(paths, path.(string))
-		case schema.ChatMessagePartTypeFileURL:
-			if message.UserInputMultiContent[i].File == nil || message.UserInputMultiContent[i].File.Extra == nil {
-				continue
-			}
-			path := message.UserInputMultiContent[i].File.Extra[ExtraKeyFilePath.String()]
-			if path == nil || path == "" {
-				continue
-			}
-			paths = append(paths, path.(string))
-		default:
-			continue
+	if extra == nil {
+		return "", false
+	}
+
+	pathValue, ok := extra[ExtraKeyFilePath.String()]
+	if !ok || pathValue == nil {
+		return "", false
+	}
+
+	path, ok := pathValue.(string)
+	if !ok || path == "" {
+		return "", false
+	}
+
+	return path, true
+}
+
+// setURLToPart 设置不同类型的 part 的 URL
+func setURLToPart(part *schema.MessageInputPart, url string) bool {
+	if url == "" {
+		return false
+	}
+
+	switch part.Type {
+	case schema.ChatMessagePartTypeImageURL:
+		if part.Image != nil {
+			part.Image.URL = &url
+			return true
+		}
+	case schema.ChatMessagePartTypeAudioURL:
+		if part.Audio != nil {
+			part.Audio.URL = &url
+			return true
+		}
+	case schema.ChatMessagePartTypeVideoURL:
+		if part.Video != nil {
+			part.Video.URL = &url
+			return true
+		}
+	case schema.ChatMessagePartTypeFileURL:
+		if part.File != nil {
+			part.File.URL = &url
+			return true
 		}
 	}
+
+	return false
+}
+
+// isFilePartType 检查是否为需要上传的文件类型
+func isFilePartType(partType schema.ChatMessagePartType) bool {
+	switch partType {
+	case schema.ChatMessagePartTypeImageURL,
+		schema.ChatMessagePartTypeAudioURL,
+		schema.ChatMessagePartTypeVideoURL,
+		schema.ChatMessagePartTypeFileURL:
+		return true
+	default:
+		return false
+	}
+}
+
+func (d *Converter) getPaths(message *schema.Message) []string {
+	if message == nil {
+		return nil
+	}
+
+	var paths []string
+	for i := range message.UserInputMultiContent {
+		part := &message.UserInputMultiContent[i]
+		if !isFilePartType(part.Type) {
+			continue
+		}
+
+		path, ok := getFilePathFromPart(part)
+		if ok {
+			paths = append(paths, path)
+		}
+	}
+
 	return paths
 }
